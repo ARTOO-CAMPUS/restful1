ee24e92ff302ffa0a1a9ff49abeca204deab44fe
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-02-21 15:43:04 +0100

    fix

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
 	$("#users").click(function () {
 		Users.getUsers().then(function (data) {
 			console.log(data);
+			var str = "";
+			for (let i = 0; i < data.length; i++) {
+				str += `<p>${data[i].name}</p>`;
+			}
+			$("#dati").html(str);
 		});
 
 	});
@@ -10,6 +15,11 @@ $(document).ready(function () {
 	$("#posts").click(function () {
 		Posts.getPosts().then(function (data) {
 			console.log(data);
+			var str = "";
+			for (let i = 0; i < data.length; i++) {
+				str += `<p>${data[i].title}</p>`;
+			}
+			$("#dati").html(str);
 		});
 
 	});
@@ -18,6 +28,11 @@ $(document).ready(function () {
 		console.log("here");
 		Posts.getComments(10).then(function (data) {
 			console.log(data);
+			var str = "";
+			for (let i = 0; i < data.length; i++) {
+				str += `<p>${data[i].body}</p>`;
+			}
+			$("#dati").html(str);
 		});
 
 	});
